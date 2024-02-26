a76582b0679078eea58da4ffa232a90b0b0f1a07
AlexxFuentes <aafuentes@unah.hn>
2024-02-25 22:51:36 -0600

    config final 1.1

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output:'export',
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'res.cloudinary.com',
-                port: '',
-                pathname: '/dn0alzand/image/upload/v1708886246/img_public/**',
-            },
-        ],
-    },
 };
 
 export default nextConfig;
